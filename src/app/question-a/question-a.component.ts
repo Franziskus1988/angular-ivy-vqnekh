4a43070076befd81e1859d995b1c9a5d9418421e
Franziskus1988 <fneuhoff@tomtec.de>
2022-02-20 21:59:53 +0100

    1 file modified

--- src/app/question-a/question-a.component.ts
+++ src/app/question-a/question-a.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService, IUser } from '../data.service';
 
-
-
 @Component({
   selector: 'app-question-a',
   template: `
@@ -11,25 +9,21 @@ import { DataService, IUser } from '../data.service';
       <li>
       </li>
     </ul>
-  `
+  `,
 })
 export class QuestionAComponent implements OnInit {
-
   /**
    * QUESTIONS:
-   * 1. How could you list the first names of all the users with Angular?
-   * 2. If there is a performance issue, how can you improve the performance?
-   * 3. You want to call the "clear" function of the DataService when leaving the component. Where could you do this? 
+   * 1. How could you list the first names of all the users stored in variable "users" with Angular?
+   * 2. If there would be a performance issue, how could you improve the performance?
+   * 3. You want to call the "clear" function of the DataService when leaving the component. Where could you do this?
    */
 
   users: IUser[] = [];
 
-
-  constructor(private dataService: DataService) { }
-
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
     this.users = this.dataService.getUsers();
   }
-
 }
